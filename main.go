2c66b1987db131b9f27032ffb25ac0a08d869a1e
longkey1 <longkey1@gmail.com>
2018-09-25 22:15:00 +0900

    Change version parameters

--- main.go
+++ main.go
@@ -83,7 +83,7 @@ func init() {
 }
 
 func checkVersion() {
-	if len(os.Args) > 1 && os.Args[1] == "version" {
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
 		fmt.Printf("fdl version %s\n", version)
 		os.Exit(0)
 	}
